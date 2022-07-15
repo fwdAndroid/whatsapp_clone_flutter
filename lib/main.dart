2471d2175dc6cbb1d982dee5c7273a04a7307e20
fwdAndroid <fwdkaleem@gmail.com>
2022-07-16 03:45:55 +0500

    Firebase Initialized

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:whatsapp_clone_flutter/colors.dart';
 import 'package:whatsapp_clone_flutter/features/landing_screen.dart';
 import 'package:whatsapp_clone_flutter/router.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
